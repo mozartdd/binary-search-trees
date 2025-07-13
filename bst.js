5972120252b6a1914fc36ca397cfa5c487c86f34
Dainis Dilevka <thedenisovan@proton.me>
2025-07-13 14:15:20 +0100

    Added function to get depth of node of given value

--- bst.js
+++ bst.js
@@ -37,6 +37,69 @@ export class Tree {
     }
     return null;
   }
+  // Returns height of node containing given value
+  height(value) {
+    let current = this.find(value);
+    let leftHeight = 0;
+    let rightHeight = 0;
+
+    if (current === null) return -1;
+    // If value is last leaf of tree
+    else if (current.left === null && current.right === null) {
+      return 0;
+    }
+    while (current.left !== null) {
+      current = current.left;
+      leftHeight++;
+    }
+    current = this.find(value);
+    while (current.right !== null) {
+      current = current.right;
+      rightHeight++;
+    }
+    return leftHeight > rightHeight ? leftHeight : rightHeight;
+  }
+  // Return depth of node with given value
+  depth(value) {
+    let current = this.root;
+    let depth = 0;
+    
+    // If not found in binary tree return -1
+    if (current === null) return -1;
+    // If value is first root node return 0 depth
+    else if (current.data === value) return depth;
+    while (current !== null && current.data !== value) {
+      if (current.data < value) {
+      current = current.right;
+      depth++;
+    } else {
+      current = current.left;
+      depth++;
+      }
+    }
+    return depth;
+  }
+}
+
+// Recursive function to construct binary tree. Source: www.geeks\for\geeks.org/sorted-array-to-balanced-bst/
+export function buildTree(arr, start, end) {
+    if (start > end) return null;
+
+    // Find the middle element
+    let mid = start + Math.floor((end - start) / 2);
+
+    // Create root node
+    let root = new Node(arr[mid]);
+
+    // Create left subtree
+    root.left = buildTree(arr, start, mid - 1);
+
+    // Create right subtree
+    root.right = buildTree(arr, mid + 1, end);
+
+    array.tree.push(root);
+    array.root = root;
+    return root;
 }
 
 function mergeSort(array) {
@@ -77,27 +140,6 @@ function checkForDuplicates(array) {
     else newArray.push(array[i]);
   }
   return newArray;
-}
-
-// Recursive function to construct binary tree. Source: www.geeks\for\geeks.org/sorted-array-to-balanced-bst/
-export function buildTree(arr, start, end) {
-    if (start > end) return null;
-
-    // Find the middle element
-    let mid = start + Math.floor((end - start) / 2);
-
-    // Create root node
-    let root = new Node(arr[mid]);
-
-    // Create left subtree
-    root.left = buildTree(arr, start, mid - 1);
-
-    // Create right subtree
-    root.right = buildTree(arr, mid + 1, end);
-
-    array.tree.push(root);
-    array.root = root;
-    return root;
 }
 
 export const prettyPrint = (node, prefix = '', isLeft = true) => {
--- main.js
+++ main.js
@@ -2,30 +2,11 @@ import * as myBst from './bst.js';
 
 
 export let array = new myBst.Tree();
-array.insert(1);
-array.insert(2);
-array.insert(3);
-array.insert(4);
-array.insert(5);
-array.insert(6);
-array.insert(7);
-array.insert(11);
-array.insert(13);
-array.insert(15);
-array.insert(21);
-array.insert(8);
-array.insert(8);
-array.insert(9);
-array.insert(0);
-array.insert(221);
-
-array.remove(0);
-array.remove(9);
-array.remove(7);
+for (let i = 0; i < 25; i++) {
+  array.insert(i);
+}
 
 myBst.prettyPrint(myBst.buildTree(array.treeData, 0, array.treeData.length - 1));
 
-console.log(array.find(13));
-
-// console.log(array.tree);
+console.log(array.depth(0));
 
